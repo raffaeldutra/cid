9c7c1740ba11828de36a7cfb0b5f1f4ad4598b98
Rafael Dutra <raffaeldutra@gmail.com>
2023-08-05 00:53:41 -0300

    Closes #70. Validação de instalação de pacotes

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -4,12 +4,30 @@ source ~/.bash_aliases
 
 source install-packages.sh
 
-install_aws
-install_kubectl
-install_helm
-install_istioctl
-install_kustomize
-install_argo
+if [ -n "${AWS_CLI_VERSION}" ]; then
+  install_aws
+fi
+
+if [ -n "${KUBECTL_VERSION}" ]; then
+  install_kubectl
+fi
+
+if [ -n "${HELM_VERSION}" ]; then
+  install_helm
+fi
+
+if [ -n "${ISTIOCTL_VERSION}" ]; then
+  install_istioctl
+fi
+
+if [ -n "${KUSTOMIZE_VERSION}" ]; then
+  install_kustomize
+fi
+
+if [ -n "${ARGO_VERSION}" ]; then
+  install_argo
+fi
+
 show_versions
 
 > ${ENV_DIRECTORY_INSTALLATION}/.finished
